5b7025eb269853925f6f8f8810378260989be360
tomaszsztajkowski <tomaszsztajkowski@gmail.com>
2023-04-03 16:59:00 +0200

    changes

--- lab5/main.py
+++ lab5/main.py
@@ -12,13 +12,15 @@ def my_printf(format_string,param):
     matches = re.split("#(\d*)g", format_string)
     if(len(matches) > 1):
         if int(matches[1]) != '':
-            param = "".join([9 if d == '0' else int(d) - 1 for d in param])
-            print(matches[0] + param[:int(matches[1])] + matches[2])
+            param = "".join(['9' if d == '0' else str(int(d) - 1) for d in param])
+            print(matches[0] + '0' * (int(matches[1]) - len(param)) + param[:int(matches[1])] + matches[2])
         return
     else:
         print(matches[0])
 
-data=sys.stdin.readlines()
+# data=sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+# for i in range(0,len(data),2):
+#     my_printf(data[i].rstrip(),data[i+1].rstrip())
+
+my_printf("test #10g test", "2345")

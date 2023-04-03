8d5e241a0d4993487289aca0cf457904e1ed9dc7
tomaszsztajkowski <tomaszsztajkowski@gmail.com>
2023-04-03 17:03:46 +0200

    uncomment

--- lab5/main.py
+++ lab5/main.py
@@ -18,9 +18,7 @@ def my_printf(format_string,param):
     else:
         print(matches[0])
 
-# data=sys.stdin.readlines()
+data=sys.stdin.readlines()
 
-# for i in range(0,len(data),2):
-#     my_printf(data[i].rstrip(),data[i+1].rstrip())
-
-my_printf("test #0g test", "2345")
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
